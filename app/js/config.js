18942ff8be53e97e265ccba148c85b712198800d
fyzhu <i@zhuyunfeng.com>
2018-03-15 11:20:07 +0800

    config

--- app/js/config.js
+++ app/js/config.js
@@ -1,16 +1,16 @@
 export const commonParams = {
     startPageNo: 1,
     endPageNo: 1,
-    pageSize: 30,
+    
     sortType: 1,
     categoryType: 1,
     languageCode: 'cs',
-    siteVersion: '7-1',
+    
     member_id: 35116189,
     marketCode: 'cn',
     partnerCode: 'Cool',
     cultureCode: 'zh-CN',
-    token: 'e508217459759f8d83e30ec18c3479c7| 2 / 5 / 2018 9: 40: 31 AM',
+    
     sourceMemberType_id: 1,
     // jsoncallback: '_jsonp_A34AB7E5085961EBA5D20136E6F0EF3A'
 }
@@ -18,7 +18,7 @@ export const commonParams = {
 export const options = {
     param: 'jsoncallback',
     // prefix: 'jp',
-    name: '_jsonp_A3056C3CADC60BFA1CF6DE6FE4846DC6'
+    name: ''
 }
 
 export const ERR_OK = 0
--- app/js/words.js
+++ app/js/words.js
@@ -6,9 +6,12 @@ export function getWords() {
     const url = 'https://ec.ef.com.cn/services/school/studytools/notebook/word/LoadWordListV2/'
 
     const data = Object.assign({}, commonParams, {
+        siteVersion: '11-1',
+        pageSize: 30,
+        token: 'ca5ec8ff0e6233d1e5c0b028614958ce| 2 / 5 / 2018 9: 40: 31 AM',
         wordGroup_id: '918'
         
     })
-
+    options.name ='_jsonp_6252A6E7EB0A7774965CDDD7B0D7BD2E';
     return jsonp(url, data, options)
 }
